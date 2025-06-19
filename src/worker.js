ee7099f7485d813edf0d9ba2300031103ef1b64f
fallec <122975561+fallec@users.noreply.github.com>
2025-06-19 19:17:51 +0300

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,15 @@
 export default {
   async fetch(req) {
     const url = new URL(req.url);
+    url.protocol = "https:";
     url.hostname = "c3278637-e408-413a-8ae6-13215785c513.cfargotunnel.com";
 
-    const newReq = new Request(url, req);
-    newReq.headers.set("Host", "anorsi.msk.ru");
+    const proxyReq = new Request(url, req);
+    proxyReq.headers.set("Host", "anorsi.msk.ru");
 
-    return fetch(newReq, {
-      cf: { resolveOverride: "origin.cloudflare.com" } // <- обход 1102
+    // обходит возможный 1102
+    return fetch(proxyReq, {
+      cf: { resolveOverride: "origin.cloudflare.com" }
     });
   }
 }
