99de66aca75bd2ea4739d75102bca703dee6657a
fallec <122975561+fallec@users.noreply.github.com>
2025-06-19 19:07:27 +0300

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,11 +1,13 @@
 export default {
-  async fetch(req, env) {
+  async fetch(req) {
     const url = new URL(req.url);
-    // подменяем origin на ваш Tunnel
     url.hostname = "c3278637-e408-413a-8ae6-13215785c513.cfargotunnel.com";
 
-    const newReq = new Request(url.toString(), req);
-    newReq.headers.set("Host", "anorsi.msk.ru");   // нужен Traefik'у
-    return fetch(newReq);
+    const newReq = new Request(url, req);
+    newReq.headers.set("Host", "anorsi.msk.ru");
+
+    return fetch(newReq, {
+      cf: { resolveOverride: "origin.cloudflare.com" } // <- обход 1102
+    });
   }
 }
